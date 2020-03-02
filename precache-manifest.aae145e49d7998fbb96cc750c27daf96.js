1aeda468f40e842b03575c7e4920cf7423162b03
Marcin Kaczor <marcin.kaczor96@gmail.com>
2020-03-02 20:05:06 +0100

    Updates

---
+++ precache-manifest.aae145e49d7998fbb96cc750c27daf96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d4d486848d70aa8da9eca8dddd7c58",
+    "url": "/the-space-game/index.html"
+  },
+  {
+    "revision": "09cc4c717fb384e746e7",
+    "url": "/the-space-game/static/css/main.2946d1b5.chunk.css"
+  },
+  {
+    "revision": "1c89d7500abc890b6cbf",
+    "url": "/the-space-game/static/js/2.a2d16be9.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/the-space-game/static/js/2.a2d16be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cc4c717fb384e746e7",
+    "url": "/the-space-game/static/js/main.0211633f.chunk.js"
+  },
+  {
+    "revision": "e58e2039c04241fe4aee",
+    "url": "/the-space-game/static/js/runtime-main.48e59352.js"
+  }
+]);
